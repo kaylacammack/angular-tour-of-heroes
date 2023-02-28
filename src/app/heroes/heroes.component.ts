0f0ee5feb268f507a82faae2d5fd963ecae2b966
Kayla Cammack <kaylacammack@gmail.com>
2023-02-27 21:09:50 -0700

    Initial Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Hero } from './../models/Hero';
+import { Component } from '@angular/core';
+import { HEROES } from 'src/mockdata/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
